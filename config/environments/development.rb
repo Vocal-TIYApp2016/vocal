dbf7e397f4b67ff106d39dd24bee0def0bb07406
ccflack <ccflack@me.com>
2016-11-28 13:05:33 -0500

    Gemfile setup; switching to branches

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,6 +26,8 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
+  # Mailer host default url (production)
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
