372c544180bb1a936445fb5272b27b7086034641
ccflack <ccflack@me.com>
2016-11-29 19:22:33 -0500

    Users passing validation. Authentication_token is returned in json response.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,13 @@
 class User < ApplicationRecord
   acts_as_token_authenticatable
+  before_validation(:on => :create) do
+    self.password_confirmation = nil
+  end
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   attachment :profile_image
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     registrations: 'users/registrations',
     sessions: 'users/sessions'
-  }
+  } do
+  post '/users' => 'users/registrations#create', as: :sign_up, constraints: {format: /(json)/}
+end
   # get '/legislators' => ''
   root 'home#static'
 
