c0e151cfea1f07b35d50eecabbe7ce33216a8a9b
ccflack <ccflack@me.com>
2016-11-30 12:17:50 -0500

    Controller changes for login/authentication with username || email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,42 @@
 class User < ApplicationRecord
-  acts_as_token_authenticatable
   before_validation(:on => :create) do
     self.password_confirmation = nil
   end
+
+  validates :username,
+  :presence => true,
+  :uniqueness => {
+    :case_sensitive => false
+  }
+
+  validates_format_of :username, with: /^[a-zA-Z0-9_\.]*$/, :multiline => true
+
+  validate :validate_username
+
+  acts_as_token_authenticatable
+  attachment :profile_image
+  attr_accessor :login
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  attachment :profile_image
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def self.find_for_database_authentication(warden_conditions)
+     conditions = warden_conditions.dup
+     if login = conditions.delete(:login)
+       where(conditions.to_h).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+     elsif conditions.has_key?(:username) || conditions.has_key?(:email)
+       where(conditions.to_h).first
+     end
+  end
+
+
+  def validate_username
+    if User.where(email: username).exists?
+      errors.add(:username, :invalid)
+    end
+  end
 
 
 end
