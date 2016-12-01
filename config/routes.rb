b7feb6424d4dd33fcc1659306508ec2519d17060
ccflack <ccflack@me.com>
2016-12-01 12:23:55 -0500

    pre-merge

--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,10 @@ Rails.application.routes.draw do
     registrations: 'users/registrations',
     sessions: 'users/sessions'
   } do
-    post '/users' => 'users/registrations#create', as: :sign_up, constraints: {format: /(json)/}
-    post '/sessions/sign_in' => 'users/sessions#create', as: :sign_in, constraints: {format: /(json)/}
-  end
+      post '/users' => 'users/registrations#create', as: :sign_up, constraints: {format: /(json)/}
+      post '/sessions/sign_in' => 'users/sessions#create', as: :sign_in, constraints: {format: /(json)/}
+    end
+
 
   get '/self' => 'users#show_self'
   get '/users/:id' => 'users#show'
