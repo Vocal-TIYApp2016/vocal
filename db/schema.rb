22afe320c4f8d986d5db143e5aac77af68a853be
ccflack <ccflack@me.com>
2016-12-05 10:19:14 -0500

    Set images to fill, no resize

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161202143350) do
+ActiveRecord::Schema.define(version: 20161204234814) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,6 +58,14 @@ ActiveRecord::Schema.define(version: 20161202143350) do
     t.datetime "created_at"
     t.index ["mentionable_id", "mentionable_type"], name: "fk_mentionables", using: :btree
     t.index ["mentioner_id", "mentioner_type"], name: "fk_mentions", using: :btree
+  end
+
+  create_table "refile_attachments", force: :cascade do |t|
+    t.integer  "oid",        null: false
+    t.string   "namespace",  null: false
+    t.datetime "created_at"
+    t.index ["namespace"], name: "index_refile_attachments_on_namespace", using: :btree
+    t.index ["oid"], name: "index_refile_attachments_on_oid", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
