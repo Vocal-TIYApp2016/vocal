ac99aaa6165385e0d7689bc205a0a916963eeff9
Sally Wieland <sallybwieland@gmail.com>
2016-12-06 22:13:23 -0500

    started follow function on legislator header

--- webpack/components/LegislatorHeader.js
+++ webpack/components/LegislatorHeader.js
@@ -9,6 +9,7 @@ class LegislatorHeader extends React.Component {
     this.updateLegislator = this.updateLegislator.bind(this)
     this.updatePhoto = this.updatePhoto.bind(this)
     this.logout = this.logout.bind(this)
+    this.follow = this.follow.bind(this)
     this.state = {
       title: "--",
       firstName: "--",
@@ -50,6 +51,18 @@ class LegislatorHeader extends React.Component {
      sessionStorage.removeItem('user_id')
   }
 
+  follow(){
+    var formData = new FormData()
+    formData.append('legislator[id]', sessionStorage.getItem('legislator_id'))
+    formData.append('user[authentication_token]', sessionStorage.getItem('api_token'))
+    fetch('/legislators/' +  sessionStorage.getItem('legislator_id') + '/follow', {
+      body: formData,
+      method: 'POST'
+    })
+    .then(response => response.json())
+    .then(response => console.log(response))
+  }
+
   render(){
     return <div>
     <div className='hiddenSection'>
@@ -75,7 +88,7 @@ class LegislatorHeader extends React.Component {
           <br />
           <h2 className="text-center profileText">{this.state.title}<br/>
           {this.state.firstName} {this.state.lastName}</h2>
-          <h5 className="text-center profileTextTwo">{this.state.party} | <button className="btn followBtn">Follow</button></h5>
+          <h5 className="text-center profileTextTwo">{this.state.party} | <button className="btn followBtn" onClick={this.follow}>Follow</button></h5>
 
         </div>
       </div>
