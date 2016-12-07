abeb712952125f88d5c7cd0d6d4857df99801d37
ccflack <ccflack@me.com>
2016-12-07 17:54:56 -0500

    followeeees

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,16 +1,16 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :authentication_token, :email, :username, :profile_image, :first_name, :last_name, :followed
+  attributes :id, :authentication_token, :email, :username, :profile_image, :first_name, :last_name #:followed
 
   def profile_image
     Refile.attachment_url(object, :profile_image, :fill, 200, 200, format: "jpg")
   end
 
-  def followed
-    if current_user
-      @legislators = current_user(followees(Legislator))
-    else
-      false
-    end
-  end
+  # def followed
+  #   if current_user
+  #     @legislators = current_user(followees(Legislator))
+  #   else
+  #     false
+  #   end
+  # end
 
 end
