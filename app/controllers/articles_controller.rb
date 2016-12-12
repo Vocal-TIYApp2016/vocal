fd75eb2fd3f532e8b673e2fae9ef3882f35c13ef
ccflack <ccflack@me.com>
2016-12-12 15:09:08 -0500

    Push frontend changes

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,23 +3,24 @@ class ArticlesController < ApplicationController
   require 'open-uri'
 
   def article_pull(sources)
-    newshash = {}
+    newshash = []
     sources.each do |url|
     feed = RSS::Parser.parse(url)
     origin = feed.channel.title
-      newshash = feed.items.map do |item|
+      articles = feed.items.map do |item|
           {title: item.title,
           link: item.link,
           description: item.description,
           source: origin,
-          date: item.pubDate }
+          date: (item.try(:pubDate) || item.dc_date) }
         end
+      newshash << articles
     end
     newshash
   end
 
   def sources
-    ['http://indypolitics.org/feed/', 'http://howeypolitics.com/RSS/0']
+    ['http://indypolitics.org/feed/', 'http://www.ibj.com/rss/40', 'http://www.ibj.com/rss/125', 'http://www.ibj.com/rss/126', 'http://howeypolitics.com/RSS/0', 'https://www.fbi.gov/feeds/indianapolis-news/RSS']
   end
 
   def index
