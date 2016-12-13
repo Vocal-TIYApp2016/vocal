7a71317477e112da6dba79ddab89af1aa51babce
ccflack <ccflack@me.com>
2016-12-13 10:06:45 -0500

    Permit address in params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,9 +7,9 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    added_attrs = [:first_name, :last_name, :username, :email, :password, :profile_image, :zip_code, :remember_me]
+    added_attrs = [:first_name, :last_name, :username, :email, :password, :profile_image, :address, :remember_me]
     devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
-    devise_parameter_sanitizer.permit :account_update, keys: [:first_name, :last_name, :username, :email, :profile_image, :zip_code]
+    devise_parameter_sanitizer.permit :account_update, keys: [:first_name, :last_name, :username, :email, :profile_image, :address]
     devise_parameter_sanitizer.permit :sign_in, keys: [:email, :password]
   end
 
