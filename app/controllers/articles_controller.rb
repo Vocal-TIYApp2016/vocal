986fb4b761451185cb5624b37e7d7b8d932782b0
ccflack <ccflack@me.com>
2016-12-13 15:56:39 -0500

    flatten

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,15 +3,15 @@ class ArticlesController < ApplicationController
   def index
     @articles = article_search
     @articles = Article.all if @articles == []
-    render json: @articles, serializer: ArticleSerializer
+    render json: @articles, each_serializer: ArticleSerializer
   end
 
   def article_search
     articles =[]
     current_user.legislators.each do |leg|
-      articles << Article.search_by_full_text(leg.full_name)
+      articles << Article.search_by_full_text(leg.full_name).to_a
     end
-    articles
+    articles.flatten
   end
 
 end
