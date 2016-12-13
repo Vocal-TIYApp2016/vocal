238f76725e3771f07e279cb4c7fc549da8ceea31
ccflack <ccflack@me.com>
2016-12-13 13:32:38 -0500

    front end changes

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,20 @@
 class ArticlesController < ApplicationController
 
+  include PgSearch
+  pg_search_scope :search_by_full_text, against: [:description, :title]
+
   def index
-    @articles = Article.all
+    @articles = article_search
+    @articles = Article.all if @articles == []
     render json: @articles
+  end
+
+  def article_search
+    articles =[]
+    current_user.legislators.each do |leg|
+      articles << Article.all.search_by_full_text(current_user.leg.full_name)
+    end
+    articles
   end
 
 end
