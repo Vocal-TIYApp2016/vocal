6f0a1bb7cf61dd61efc2d7eac9619128e8aeb850
ccflack <ccflack@me.com>
2016-12-14 16:39:55 -0500

    adding cdn

--- config/initializers/refile.rb
+++ config/initializers/refile.rb
@@ -3,3 +3,5 @@ Refile.configure do |config|
   connection = lambda { |&blk| ActiveRecord::Base.connection_pool.with_connection { |con| blk.call(con.raw_connection) } }
   config.store = Refile::Postgres::Backend.new(connection)
 end
+
+Refile.cdn_host = 'https://d2tgzqkelel7sa.cloudfront.net'
